c13e5e6f4b34bd842490ceaac093a94d838c2307
shengnwen <shengwen@live.cn>
2015-11-10 19:46:25 -0500

    11.10 meeting

--- 10-27/csvExtract.py
+++ 10-27/csvExtract.py
@@ -46,28 +46,53 @@ def extractSubjects(csvFile="./allDutch.csv"):
             result += (str(ele) + "," + str(dict[ele]) + "\n")
         f.writelines(result)
 # 3. parse subjects
-def classifySubjects(csvFile="./allDutch-subject.csv"):
+# def classifySubjects(csvFile="./allDutch-subject.csv"):
+#     dict = {}
+#     with open(csvFile) as f:
+#         data = [row for row in csv.reader(f.read().splitlines())]
+#         for row in data[1:]:
+#             ele = row[0].strip()
+#             count = row[1].strip()
+#             if ele == None or ele == '' or ele == 'undefined':
+#                 continue
+#             words = ele.split('|')
+#             for word in words:
+#                 word = word.strip()
+#                 if word in dict:
+#                     dict[word] += count
+#                 else:
+#                     dict[word] = count
+#     for ele in sorted(dict):
+#         print ele, dict[ele]
+#     ele_filename = csvFile[:-4] + "-classified.csv"
+#     print("create classified subject file:" + ele_filename)
+#     with open(ele_filename, 'w') as f:
+#         result = 'Keyword, Count\n'
+#         for ele in sorted(dict):
+#             result += (str(ele) + "," + str(dict[ele]) + "\n")
+#         f.writelines(result)
+
+def classifySubjects(csvFile="./allDutch.csv"):
     dict = {}
     with open(csvFile) as f:
         data = [row for row in csv.reader(f.read().splitlines())]
         for row in data[1:]:
-            ele = row[0].strip()
-            count = row[1].strip()
+            ele = row[6].strip().rstrip(',')
             if ele == None or ele == '' or ele == 'undefined':
                 continue
-            words = ele.split('|')
-            for word in words:
-                word = word.strip()
-                if word in dict:
-                    dict[word] += count
+            # ele = ele.replace(',', '|')
+            eles = ele.split(',')
+            for element in eles:
+                if element in dict:
+                    dict[element] += 1
                 else:
-                    dict[word] = count
-    for ele in sorted(dict):
-        print ele, dict[ele]
-    ele_filename = csvFile[:-4] + "-classified.csv"
-    print("create classified subject file:" + ele_filename)
+                    dict[element] = 1
+    for element in sorted(dict):
+        print element, dict[element]
+    ele_filename = csvFile[:-4] + "-subject-classified.csv"
+    print("create subject file:" + ele_filename)
     with open(ele_filename, 'w') as f:
-        result = 'Keyword, Count\n'
+        result = 'Subject, Count\n'
         for ele in sorted(dict):
             result += (str(ele) + "," + str(dict[ele]) + "\n")
         f.writelines(result)
@@ -76,5 +101,64 @@ def classifySubjects(csvFile="./allDutch-subject.csv"):
 # extractAuthor("English_MMW_1580 1720.csv")
 # extractSubjects("allDutch.csv")
 # extractSubjects("English_MMW_1580 1720.csv")
-classifySubjects("allDutch-subject.csv")
-classifySubjects("English_MMW_1580 1720-subject.csv")
+# classifySubjects("allDutch-subject.csv")
+# classifySubjects("English_MMW_1580 1720-subject.csv")
+
+def extractKeyWord(csvFile = "./allDutch.csv",keywords=["Company", "Companies", "Compagnie", "Corporation","Corporations"]):
+    dict = {}
+    with open(csvFile) as f:
+        data = [row for row in csv.reader(f.read().splitlines())]
+        for row in data[1:]:
+            ele = row[6].strip().rstrip(',')
+            if ele == None or ele == '' or ele == 'undefined':
+                continue
+            ele = ele.replace(',', ' ')
+            # eles = ele.split(",")
+            # for element in eles:
+            for key in keywords:
+                if key in ele:
+                    if ele in dict:
+                        dict[ele] += 1
+                    else:
+                        dict[ele] = 1
+    for ele in sorted(dict):
+        print ele, dict[ele]
+    ele_filename = csvFile[:-4] + "-subject-keywords-"+keywords[0]+".csv"
+    print("create subject file:" + ele_filename)
+    with open(ele_filename, 'w') as f:
+        result = 'Key:' + keywords[0] + ', Count\n'
+        for ele in sorted(dict):
+            result += (str(ele) + "," + str(dict[ele]) + "\n")
+        f.writelines(result)
+
+# special for deuth
+def extractKeyWordClassiefied(csvFile = "./allDutch.csv",keywords=["Company", "Companies", "Compagnie", "Corporation","Corporations"]):
+    dict = {}
+    with open(csvFile) as f:
+        data = [row for row in csv.reader(f.read().splitlines())]
+        for row in data[1:]:
+            ele = row[6].strip().rstrip(',')
+            if ele == None or ele == '' or ele == 'undefined':
+                continue
+            #ele = ele.replace(',', ' ')
+            eles = ele.split(",")
+            for element in eles:
+                for key in keywords:
+                    if key in element:
+                        if element in dict:
+                            dict[element] += 1
+                        else:
+                            dict[element] = 1
+    for ele in sorted(dict):
+        print ele, dict[ele]
+    ele_filename = csvFile[:-4] + "-subject-keywords-"+keywords[0]+".csv"
+    print("create subject file:" + ele_filename)
+    with open(ele_filename, 'w') as f:
+        result = 'Key:' + keywords[0] + ', Count\n'
+        for ele in sorted(dict):
+            result += (str(ele) + "," + str(dict[ele]) + "\n")
+        f.writelines(result)
+
+extractKeyWord("./English_MMW_1580 1720.csv")
+extractKeyWordClassiefied()
+classifySubjects("./English_MMW_1580 1720.csv")
